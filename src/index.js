c9fa60e25095f898fbd1e7080bea3af50742ee96
john mclaughlin <john.n.mclaughlin@gmail.com>
2017-09-11 15:49:52 -0400

    added carousel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 require('./assets/stylesheets/styles.scss');
- 
+
+//import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Masthead from './app/Masthead.jsx';
+
+ReactDOM.render(<Masthead />, document.getElementById('masthead'));
